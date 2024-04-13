486efcac1c1e1d0f56fe7f7cfafd8a83f3e30eec
Avain <sports113499@gmail.com>
2024-04-13 23:15:42 +0800

    feat:Day15 task

--- src/App.js
+++ src/App.js
@@ -4,8 +4,26 @@ import {
   NavLink,
   Routes,
   Route,
-  useNavigate
+  useNavigate,
+  useParams,
+  Outlet
 } from 'react-router-dom';
+
+const PostId = () => {
+  const {postId} = useParams();
+  return (
+    <p>Post ID : {postId}</p>
+  );
+};
+
+const Post = () => {
+  return (
+    <>
+      <h3>Post 頁面</h3>
+      <Outlet />
+    </>
+  );
+}
 
 const Logout = () => {
   const navigate = useNavigate();
@@ -46,12 +64,18 @@ function App() {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/post/post123">
+            <p>Post 詳細頁面</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
         <Routes>
           <Route path='register' element={<Register />} />
           <Route path='login' element={<Login />} />
           <Route path='todo' element={<Todo />} />
+          <Route path='post' element={<Post />} >
+            <Route path=':postId' element={<PostId />} />
+          </Route>
         </Routes>
         {/* 練習區 */}
       </HashRouter>
